99b7c623a4b8ab2d11aafc43c6f055256638e9ff
TigerXHero <isaiastufatherx@gmail.com>
2021-09-24 12:29:55 -0400

    Update AloMundo.java

--- AloMundo.java
+++ AloMundo.java
@@ -3,7 +3,7 @@ public class AloMundo
 	public static void main(String args[])
 	{
 		System.out.println("Alo Mundo");
-		System.out.println("Ivan rata");
+		System.out.println("Ivancetetest");
 		System.out.println("Respondeeeeee");
 	}
 }
